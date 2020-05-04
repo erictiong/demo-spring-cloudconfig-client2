2ad831bd442df67943c007d542690f5d0e7f7329
erictiong <64233298+erictiong@users.noreply.github.com>
2020-05-04 10:14:27 -0500

    Initial commit

---
+++ src/main/java/com/example/demospringcloudconfigclient2/DemoSpringCloudconfigClient2Application.java
@@ -0,0 +1,14 @@
+package com.example.demospringcloudconfigclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class DemoSpringCloudconfigClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudconfigClient2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringcloudconfigclient2/DemoSpringCloudconfigClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringcloudconfigclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudconfigClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
